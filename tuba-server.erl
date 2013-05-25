b4b19ff0797e179777ef5f2ec2e79ebca78704de
Raffael Schmid <raffael@yux.ch>
2013-05-25 14:27:50 +0200

    fix?

--- tuba-server.erl
+++ tuba-server.erl
@@ -6,14 +6,14 @@ start() ->
 
 server(Port) ->
     {ok, Socket} = gen_udp:open(Port, [binary, {active, false}]),
-    io:format("server opened socket:~p~n",[Socket]),
+    % io:format("server opened socket:~p~n",[Socket]),
     loop(Socket).
 
 loop(Socket) ->
     inet:setopts(Socket, [{active, once}]),
     receive
         {udp, Socket, Host, Port, Bin} ->
-            io:format("server received:~p~n",[Bin]),
+            % io:format("server received:~p~n",[Bin]),
             gen_udp:send(Socket, Host, Port, Bin),
             loop(Socket)
     end.
