093ce554d3f395ef5778d14c52c195fdb01606eb
Raffael Schmid <raffael@yux.ch>
2013-05-25 14:21:08 +0200

    try integrating our udp server

--- src/tuba_server.erl
+++ src/tuba_server.erl
@@ -1,43 +1,8 @@
 -module(tuba_server).
 
--behaviour(gen_server).
+-export([init/0]).
 
--export([start_link/0, say_hello/0]).
-
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
-
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
-
-init([]) ->
-    {ok, []}.
-
-say_hello() ->
-    gen_server:call(?MODULE, hello).
-
-
-%% callbacks
-handle_call(hello, _From, State) ->
-    io:format("Hello from server!~n", []),
-    {reply, ok, State};
-
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
-
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-start() ->
+init() ->
     spawn(fun() -> server(4000) end).
 
 server(Port) ->
@@ -53,4 +18,3 @@ loop(Socket) ->
             gen_udp:send(Socket, Host, Port, Bin),
             loop(Socket)
     end.
-
