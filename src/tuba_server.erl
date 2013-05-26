60143a0829b92a1fcf8a16b6e41ae5e40d03c47c
Raffael Schmid <raffael@yux.ch>
2013-05-26 07:14:03 +0200

    fix?

--- src/tuba_server.erl
+++ src/tuba_server.erl
@@ -1,70 +1,149 @@
 -module(tuba_server).
 
--behaviour(gen_server).
+-behaviour(gen_fsm).
 
--define(SERVER, ?MODULE).
+-export([start_link/0, set_socket/2]).
 
-%% ------------------------------------------------------------------
-%% API Function Exports
-%% ------------------------------------------------------------------
+%% gen_fsm callbacks
+-export([init/1, handle_event/3,
+         handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
 
--export([start_link/0]).
+%% FSM States
+-export([
+    'WAIT_FOR_SOCKET'/2,
+    'WAIT_FOR_DATA'/2
+]).
 
-%% ------------------------------------------------------------------
-%% gen_server Function Exports
-%% ------------------------------------------------------------------
+-record(state, {
+                socket,    % client socket
+                addr       % client address
+               }).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-define(TIMEOUT, 120000).
 
-%% ------------------------------------------------------------------
-%% API Function Definitions
-%% ------------------------------------------------------------------
+%%%------------------------------------------------------------------------
+%%% API
+%%%------------------------------------------------------------------------
 
+%%-------------------------------------------------------------------------
+%% @spec (Socket) -> {ok,Pid} | ignore | {error,Error}
+%% @doc To be called by the supervisor in order to start the server.
+%%      If init/1 fails with Reason, the function returns {error,Reason}.
+%%      If init/1 returns {stop,Reason} or ignore, the process is
+%%      terminated and the function returns {error,Reason} or ignore,
+%%      respectively.
+%% @end
+%%-------------------------------------------------------------------------
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_fsm:start_link(?MODULE, [], []).
 
-%% ------------------------------------------------------------------
-%% gen_server Function Definitions
-%% ------------------------------------------------------------------
+set_socket(Pid, Socket) when is_pid(Pid), is_port(Socket) ->
+    gen_fsm:send_event(Pid, {socket_ready, Socket}).
 
-init(Args) ->
-    {ok, Args}.
+%%%------------------------------------------------------------------------
+%%% Callback functions from gen_server
+%%%------------------------------------------------------------------------
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+%%-------------------------------------------------------------------------
+%% Func: init/1
+%% Returns: {ok, StateName, StateData}          |
+%%          {ok, StateName, StateData, Timeout} |
+%%          ignore                              |
+%%          {stop, StopReason}
+%% @private
+%%-------------------------------------------------------------------------
+init([]) ->
+    process_flag(trap_exit, true),
+    {ok, 'WAIT_FOR_SOCKET', #state{}}.
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+%%-------------------------------------------------------------------------
+%% Func: StateName/2
+%% Returns: {next_state, NextStateName, NextStateData}          |
+%%          {next_state, NextStateName, NextStateData, Timeout} |
+%%          {stop, Reason, NewStateData}
+%% @private
+%%-------------------------------------------------------------------------
+'WAIT_FOR_SOCKET'({socket_ready, Socket}, State) when is_port(Socket) ->
+    % Now we own the socket
+    inet:setopts(Socket, [{active, once}, {packet, 2}, binary]),
+    {ok, {IP, _Port}} = inet:peername(Socket),
+    {next_state, 'WAIT_FOR_DATA', State#state{socket=Socket, addr=IP}, ?TIMEOUT};
+'WAIT_FOR_SOCKET'(Other, State) ->
+    error_logger:error_msg("State: 'WAIT_FOR_SOCKET'. Unexpected message: ~p\n", [Other]),
+    %% Allow to receive async messages
+    {next_state, 'WAIT_FOR_SOCKET', State}.
 
-handle_info(_Info, State) ->
-    {noreply, State}.
+%% Notification event coming from client
+'WAIT_FOR_DATA'({data, Data}, #state{socket=S} = State) ->
+    ok = gen_udp:send(S, Data),
+    {next_state, 'WAIT_FOR_DATA', State, ?TIMEOUT};
 
-terminate(_Reason, _State) ->
+'WAIT_FOR_DATA'(timeout, State) ->
+    error_logger:error_msg("~p Client connection timeout - closing.\n", [self()]),
+    {stop, normal, State};
+
+'WAIT_FOR_DATA'(Data, State) ->
+    io:format("~p Ignoring data: ~p\n", [self(), Data]),
+    {next_state, 'WAIT_FOR_DATA', State, ?TIMEOUT}.
+
+%%-------------------------------------------------------------------------
+%% Func: handle_event/3
+%% Returns: {next_state, NextStateName, NextStateData}          |
+%%          {next_state, NextStateName, NextStateData, Timeout} |
+%%          {stop, Reason, NewStateData}
+%% @private
+%%-------------------------------------------------------------------------
+handle_event(Event, StateName, StateData) ->
+    {stop, {StateName, undefined_event, Event}, StateData}.
+
+%%-------------------------------------------------------------------------
+%% Func: handle_sync_event/4
+%% Returns: {next_state, NextStateName, NextStateData}            |
+%%          {next_state, NextStateName, NextStateData, Timeout}   |
+%%          {reply, Reply, NextStateName, NextStateData}          |
+%%          {reply, Reply, NextStateName, NextStateData, Timeout} |
+%%          {stop, Reason, NewStateData}                          |
+%%          {stop, Reason, Reply, NewStateData}
+%% @private
+%%-------------------------------------------------------------------------
+handle_sync_event(Event, _From, StateName, StateData) ->
+    {stop, {StateName, undefined_event, Event}, StateData}.
+
+%%-------------------------------------------------------------------------
+%% Func: handle_info/3
+%% Returns: {next_state, NextStateName, NextStateData}          |
+%%          {next_state, NextStateName, NextStateData, Timeout} |
+%%          {stop, Reason, NewStateData}
+%% @private
+%%-------------------------------------------------------------------------
+handle_info({udp, Socket, Bin}, StateName, #state{socket=Socket} = StateData) ->
+    % Flow control: enable forwarding of next TCP message
+    inet:setopts(Socket, [{active, once}]),
+    ?MODULE:StateName({data, Bin}, StateData);
+
+handle_info({udp_closed, Socket}, _StateName,
+            #state{socket=Socket, addr=Addr} = StateData) ->
+    error_logger:info_msg("~p Client ~p disconnected.\n", [self(), Addr]),
+    {stop, normal, StateData};
+
+handle_info(_Info, StateName, StateData) ->
+    {noreply, StateName, StateData}.
+
+%%-------------------------------------------------------------------------
+%% Func: terminate/3
+%% Purpose: Shutdown the fsm
+%% Returns: any
+%% @private
+%%-------------------------------------------------------------------------
+terminate(_Reason, _StateName, #state{socket=Socket}) ->
+    (catch gen_udp:close(Socket)),
     ok.
 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-%% ------------------------------------------------------------------
-%% Internal Function Definitions
-%% ------------------------------------------------------------------
-
-
-%start() ->
-%    spawn(fun() -> server(4000) end).
-%
-%server(Port) ->
-%    {ok, Socket} = gen_udp:open(Port, [binary, {active, false}]),
-%    io:format("server opened socket:~p~n",[Socket]),
-%    loop(Socket).
-%
-%loop(Socket) ->
-%    inet:setopts(Socket, [{active, once}]),
-%    receive
-%        {udp, Socket, Host, Port, Bin} ->
-%            io:format("server received:~p~n",[Bin]),
-%            gen_udp:send(Socket, Host, Port, Bin),
-%            loop(Socket)
-%    end.
-
+%%-------------------------------------------------------------------------
+%% Func: code_change/4
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState, NewStateData}
+%% @private
+%%-------------------------------------------------------------------------
+code_change(_OldVsn, StateName, StateData, _Extra) ->
+    {ok, StateName, StateData}.
--- src/tubaserver_app.erl
+++ src/tubaserver_app.erl
@@ -2,15 +2,84 @@
 
 -behaviour(application).
 
+%% Internal API
+-export([start_client/0]).
+
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/2, stop/1, init/1]).
+
+-define(MAX_RESTART,    5).
+-define(MAX_TIME,      60).
+-define(DEF_PORT,    4000).
+
+%% A startup function for spawning new client connection handling FSM.
+%% To be called by the TCP listener process.
+start_client() ->
+    supervisor:start_child(tubaclient_sup, []).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    tubaserver_sup:start_link().
+    ListenPort = get_app_env(listen_port, ?DEF_PORT),
+    supervisor:start_link({local, ?MODULE}, ?MODULE, [ListenPort, tubaserver_fsm]).
 
 stop(_State) ->
     ok.
+
+%%----------------------------------------------------------------------
+%% Supervisor behaviour callbacks
+%%----------------------------------------------------------------------
+init([Port, Module]) ->
+    {ok,
+        {_SupFlags = {one_for_one, ?MAX_RESTART, ?MAX_TIME},
+            [
+              % TCP Listener
+              {   tubaserver_sup,                          % Id       = internal id
+                  {udp_listener,start_link,[Port,Module]}, % StartFun = {M, F, A}
+                  permanent,                               % Restart  = permanent | transient | temporary
+                  2000,                                    % Shutdown = brutal_kill | int() >= 0 | infinity
+                  worker,                                  % Type     = worker | supervisor
+                  [udp_listener]                           % Modules  = [Module] | dynamic
+              },
+              % Client instance supervisor
+              {   tubaclient_sup,
+                  {supervisor,start_link,[{local, tubaclient_sup}, ?MODULE, [Module]]},
+                  permanent,                               % Restart  = permanent | transient | temporary
+                  infinity,                                % Shutdown = brutal_kill | int() >= 0 | infinity
+                  supervisor,                              % Type     = worker | supervisor
+                  []                                       % Modules  = [Module] | dynamic
+              }
+            ]
+        }
+    };
+
+init([Module]) ->
+    {ok,
+        {_SupFlags = {simple_one_for_one, ?MAX_RESTART, ?MAX_TIME},
+            [
+              % TCP Client
+              {   undefined,                               % Id       = internal id
+                  {Module,start_link,[]},                  % StartFun = {M, F, A}
+                  temporary,                               % Restart  = permanent | transient | temporary
+                  2000,                                    % Shutdown = brutal_kill | int() >= 0 | infinity
+                  worker,                                  % Type     = worker | supervisor
+                  []                                       % Modules  = [Module] | dynamic
+              }
+            ]
+        }
+    }.
+
+%%----------------------------------------------------------------------
+%% Internal functions
+%%----------------------------------------------------------------------
+get_app_env(Opt, Default) ->
+    case application:get_env(application:get_application(), Opt) of
+    {ok, Val} -> Val;
+    _ ->
+        case init:get_argument(Opt) of
+        [[Val | _]] -> Val;
+        error       -> Default
+        end
+    end.
